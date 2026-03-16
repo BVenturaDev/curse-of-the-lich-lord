fdb2a50ddd2b19454a36f224bc73ba84886a180f
BVenturaDev <oeslian@gmail.com>
2026-03-15 22:50:21 -0400

    State Machine Setup

---
+++ src/entities/characters/state.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name State
+
+signal ChangeState
+
+func Enter() -> void:
+	pass
+	
+func Exit() -> void:
+	pass
+
+func Update(_delta: float) -> void:
+	pass
+
+func Physics_Update(_delta: float) -> void:
+	pass
---
+++ src/entities/state_machine.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var current_state: State
+var states: Dictionary = {}
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.ChangeState.connect(on_change_state)
+
+func _process(delta: float) -> void:
+	if current_state:
+		current_state.Update(delta)
+		
+func _physics_process(delta: float) -> void:
+	if current_state:
+		current_state.Physics_Update(delta)
+
+func on_change_state(state: State, new_state_name: String) -> void:
+	if state != current_state:
+		return
+	var new_state = states.get(new_state_name.to_lower())
+	if !new_state:
+		return
+	
+	if current_state:
+		state.Exit()
+	
+	new_state.Enter()
+	current_state = new_state
