0553e51ace353b8df3cef387844365162d6f4d8b
BVenturaDev <oeslian@gmail.com>
2026-03-18 21:16:07 -0400

    death skeletons no longer scale

--- src/util/dungeon.gd
+++ src/util/dungeon.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	for i in range(0, Gamestate.death_positions.size()):
 		var new_skele = skeleton.instantiate()
 		add_child(new_skele)
-		new_skele.scale = Vector3(1.25, 1.25, 1.25)
+		#new_skele.scale = Vector3(1.25, 1.25, 1.25)
 		new_skele.global_position = Gamestate.death_positions[i]
 		new_skele.health = Gamestate.death_health[i]
 		new_skele.death_light.visible = true
